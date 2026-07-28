d7ef7d51c422d7ebe554347b06d2d994d2101898
Hanu <hanubabbar321@gmail.com>
2026-07-28 21:49:02 +0530

    ver

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
         applicationId = "com.rogue.brainrottracker"
         minSdk = 24
         targetSdk = 36
-        versionCode = 9
-        versionName = "0.934"
+        versionCode = 11
+        versionName = "0.936"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
